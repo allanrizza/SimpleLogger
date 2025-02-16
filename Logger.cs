fa1fe558591165ad997bded20efd25faa3ae3d17
Allan Rizza <allanrizza.dev@gmail.com>
2025-02-16 12:55:51 -0300

    Add project files.

---
+++ Logger.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleLogger
+{
+    public class Logger
+    {
+        private static readonly string logDirectory = "Logs";
+        private static readonly string logFilePath = Path.Combine(logDirectory, "log.txt");
+
+        static Logger()
+        {
+            if (!Directory.Exists(logDirectory))
+                Directory.CreateDirectory(logDirectory);
+        }
+
+        public static void Log(string message)
+        {
+            try
+            {
+                string logEntry = $"{DateTime.Now:yyyy-MM-dd HH:mm:ss} ---> {message}";
+                File.AppendAllText(logFilePath, logEntry + Environment.NewLine);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Erro ao escrever no log: {ex.Message}");
+            }
+        }
+    }
+}
