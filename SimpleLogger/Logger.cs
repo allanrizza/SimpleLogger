8e20b754bf4098add7b8ee1e1145eb322e4488fb
Allan Rizza <allanrizza.dev@gmail.com>
2025-02-16 21:31:21 -0300

    refactoring directory creation and changing private variables names

--- SimpleLogger/Logger.cs
+++ SimpleLogger/Logger.cs
@@ -9,21 +9,35 @@ namespace SimpleLogger
 {
     public class Logger
     {
-        private static readonly string logDirectory = "Logs";
-        private static readonly string logFilePath = Path.Combine(logDirectory, "log.txt");
+        private static readonly string _logDirectory = "Logs";
+        private static readonly string _logFilePath = Path.Combine(_logDirectory, "log.txt");
 
         public static void Log(string message)
         {
             try
             {
-                if (!Directory.Exists(logDirectory))
-                    Directory.CreateDirectory(logDirectory);
+                CreateLogsDirectoryIfNotExists();
                 string logEntry = $"{DateTime.Now:yyyy-MM-dd HH:mm:ss} ---> {message}";
-                File.AppendAllText(logFilePath, logEntry + Environment.NewLine);
+                File.AppendAllText(_logFilePath, logEntry + Environment.NewLine);
             }
             catch(Exception ex)
             {
                 Console.WriteLine($"Erro ao escrever no log: {ex.Message}");
+            }
+        }
+
+        public static void CreateLogsDirectoryIfNotExists()
+        {
+            try
+            {
+                if (!Directory.Exists(_logDirectory))
+                {
+                    Directory.CreateDirectory(_logDirectory);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erro ao criar diretório de Logs: {ex.Message}");
             }
         }
     }
